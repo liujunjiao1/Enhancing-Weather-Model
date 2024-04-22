65468a15cf13fc4d63aad51d4c1c27f88eb51729
liujunjiao1 <46129066+liujunjiao1@users.noreply.github.com>
2024-04-22 11:27:50 +0800

    buffer.py

--- buffer.py
+++ buffer.py
@@ -4,7 +4,7 @@ import pdb
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from data.bufferDataGet import *
+from bufferDataGet import *
 
 class Buffer(nn.Module):
     def __init__(self, folder, buffer):
